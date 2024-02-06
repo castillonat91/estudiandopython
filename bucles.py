59e60389bcc0e261cb26dc52c4c8c9095015549e
nataliacastillo <castillonat91@gmail.com>
2024-02-06 08:51:16 -0500

    ejercicio con el ciclo while

--- bucles.py
+++ bucles.py
@@ -1,1 +1,21 @@
-print("bucler.py")
+import random
+
+secreto = random.randint(1,10)
+
+numero = int(input("adivinar el numero entre 1 a 10"))
+
+while numero != secreto:
+    if numero < secreto:
+        print("el numero es mayo")
+    else:
+        print("el numero es menor")
+
+    numero = int(input("intenta de nuevo :"))
+
+print("felicitaciones adivinaste el numero secreto :",secreto)
+
+
+
+
+
+
